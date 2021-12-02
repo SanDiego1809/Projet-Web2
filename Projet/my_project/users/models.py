10041c1daabc672f0c7096d98dd963fd73e1b310
Sanchez Diego <sanchezdiego1809@gmail.com>
2021-12-02 10:54:27 +0100

    mélange filtre+watchlist

--- Projet/my_project/users/models.py
+++ Projet/my_project/users/models.py
@@ -19,3 +19,4 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size) #resize the image to 300x300
             img.save(self.image.path) #save the changes
+
