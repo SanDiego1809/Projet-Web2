3ad1168f159263274d5dea6b45167c97d6561b67
Sanchez Diego <sanchezdiego1809@gmail.com>
2021-12-09 17:25:14 +0100

    fix

--- Projet/my_project/blog/urls.py
+++ Projet/my_project/blog/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('place_search/', views.place_search, name='place-search'),#chercher un endroit
     path('filter/', views.filter, name='blog-filter'),#chercher un filtre
     path('my_posts/', UserMyPostsListView.as_view(), name='user-myposts'), #permet de consulter "My posts"
-    path('map/', views.map, name='blog-map'),  # chercher un filtre
+    path('map/', views.map, name='blog-map'),
     path('my_preferences_posts/', views.preferences_posts, name='user-mypreferences'),
     path('my_preferences_posts/', views.preferences_posts, name='user-mypreferences'),
 ]
